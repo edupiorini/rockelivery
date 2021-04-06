67503874459ff9c268d6e102512482df17eac5f0
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-04-06 04:27:57 -0300

    tables information inserted

--- priv/repo/migrations/20210406071943_create_users_table.exs
+++ priv/repo/migrations/20210406071943_create_users_table.exs
@@ -2,6 +2,19 @@ defmodule Rockelivery.Repo.Migrations.CreateUsersTable do
   use Ecto.Migration
 
   def change do
+    create table :users do
+      add :adress, :string
+      add :age, :integer
+      add :cep, :string
+      add :cpf, :string
+      add :email, :string
+      add :name, :string
+      add :password_hash, :string
+
+      timestamps()
+    end
+    create unique_index(:users, [:cpf])
+    create unique_index(:users, [:email]) #criamos separados para trataros os erros separadamente
 
   end
 end
