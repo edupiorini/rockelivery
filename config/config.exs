b9fa60fa5fbced558742d11c7534dd950a0f4cd9
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-04-06 04:30:25 -0300

    uuid generetion configured

--- config/config.exs
+++ config/config.exs
@@ -10,6 +10,11 @@ use Mix.Config
 config :rockelivery,
   ecto_repos: [Rockelivery.Repo]
 
+config :rockelivery, Rockelivry.Repo,
+  # configuração para gerar uuid nessas chaves
+  migration_primary_key: [type(:binary_id)],
+  migration_foreign_key: [type(:binary_id)]
+
 # Configures the endpoint
 config :rockelivery, RockeliveryWeb.Endpoint,
   url: [host: "localhost"],
