173b47ad1b8a9a23faddd9b43c676332cb3c9a0d
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-04-06 06:47:03 -0300

    typo fixing on id generation

--- config/config.exs
+++ config/config.exs
@@ -10,7 +10,7 @@ use Mix.Config
 config :rockelivery,
   ecto_repos: [Rockelivery.Repo]
 
-config :rockelivery, Rockelivry.Repo,
+config :rockelivery, Rockelivery.Repo,
   # configuração para gerar uuid nessas chaves
   migration_primary_key: [type: :binary_id],
   migration_foreign_key: [type: :binary_id]
