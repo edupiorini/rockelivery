d6d053b593dcbd9175d82510a243fd023098836c
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-04-08 06:17:30 -0300

    atom error was missing, fixed

--- lib/rockelivery/users/delete.ex
+++ lib/rockelivery/users/delete.ex
@@ -4,14 +4,14 @@ defmodule Rockelivery.Users.Delete do
 
   def call(id) do
     case UUID.cast(id) do
-      :error -> Error.build_id_format_error()
+      :error -> {:error, Error.build_id_format_error()}
       {:ok, uuid} -> delete(uuid)
     end
   end
 
   defp delete(id) do
     case Repo.get(User, id) do
-      nil -> Error.build_user_not_found_error()
+      nil -> {:error, Error.build_user_not_found_error()}
       user -> Repo.delete(user)
     end
   end
--- lib/rockelivery/users/get.ex
+++ lib/rockelivery/users/get.ex
@@ -4,14 +4,14 @@ defmodule Rockelivery.Users.Get do
 
   def by_id(id) do
     case UUID.cast(id) do
-      :error -> Error.build_id_format_error()
+      :error -> {:error, Error.build_id_format_error()}
       {:ok, uuid} -> get(uuid)
     end
   end
 
   defp get(id) do
     case Repo.get(User, id) do
-      nil -> Error.build_user_not_found_error()
+      nil -> {:error, Error.build_user_not_found_error()}
       user -> {:ok, user}
     end
   end
