4469f2cc0e826d22b55d6c07c881f38f6ceb0e61
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-04-09 06:57:08 -0300

    refactoring due to plug use

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -5,7 +5,7 @@ defmodule Rockelivery do
   alias Rockelivery.Users.Update, as: UserUpdate
 
   defdelegate create_user(params), to: UserCreate, as: :call
-  defdelegate get_user_by_id(id), to: UserGet, as: :by_id
+  defdelegate get_user_by_id(id), to: UserGet, as: :get
   defdelegate delete_user(id), to: UserDelete, as: :call
   defdelegate update_user(params), to: UserUpdate, as: :call
 end
