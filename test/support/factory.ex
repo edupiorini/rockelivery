341660d4c8948ed1ae0ecdd366d03be776610269
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-04-13 06:29:03 -0300

    user factory and insertino on DB via factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,5 +1,7 @@
 defmodule Rockelivery.Factory do
-  use ExMachina
+  use ExMachina.Ecto, repo: Rockelivery.Repo
+
+  alias Rockelivery.User
 
   def user_params_factory do
     %{
@@ -22,6 +24,19 @@ defmodule Rockelivery.Factory do
       "email" => "teste@teste.com",
       "password" => "123456",
       "name" => "Eduardo"
+    }
+  end
+
+  def user_factory do
+    %User{
+      age: 28,
+      adress: "rua de teste, 25",
+      cep: "12070350",
+      cpf: "37779593835",
+      email: "teste@teste.com",
+      password: "123456",
+      name: "Eduardo",
+      id: "58edadfd-460a-4698-9ef2-195b67eca239"
     }
   end
 end
