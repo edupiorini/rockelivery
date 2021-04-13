b5731cfe41b40a39c7c5c11667271ea053b5cf95
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-04-13 08:43:14 -0300

    users view test ok

---
+++ test/rockelivery_web/views/users_view_test.exs
@@ -0,0 +1,31 @@
+defmodule RockeliveryWeb.UsersViewTest do
+  use RockeliveryWeb.ConnCase, async: true
+
+  import Phoenix.View
+  import Rockelivery.Factory
+
+  alias RockeliveryWeb.UsersView
+
+  test "renders create.json" do
+    user = build(:user)
+
+    response = render(UsersView, "create.json", user: user)
+
+    assert %{
+             message: "User created",
+             user: %Rockelivery.User{
+               adress: "rua de teste, 25",
+               age: 28,
+               cep: "12070350",
+               cpf: "37779593835",
+               email: "teste@teste.com",
+               id: "58edadfd-460a-4698-9ef2-195b67eca239",
+               inserted_at: nil,
+               name: "Eduardo",
+               password: "123456",
+               password_hash: nil,
+               updated_at: nil
+             }
+           } = response
+  end
+end
