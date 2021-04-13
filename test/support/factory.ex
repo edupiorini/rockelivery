3809163bf0103d5f3203115b0cd5e0260b6c398f
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-04-13 06:16:11 -0300

    factory using string format instead of atom

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -12,4 +12,16 @@ defmodule Rockelivery.Factory do
       name: "Eduardo"
     }
   end
+
+  def stringuser_params_factory do
+    %{
+      "age" => 28,
+      "adress" => "rua de teste, 25",
+      "cep" => "12070350",
+      "cpf" => "37779593835",
+      "email" => "teste@teste.com",
+      "password" => "123456",
+      "name" => "Eduardo"
+    }
+  end
 end
