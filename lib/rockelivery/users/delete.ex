395928fae3192737a91f3357d984f273e251549a
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-04-19 05:21:43 -0300

    casting was missing, no id format is veryfied

--- lib/rockelivery/users/delete.ex
+++ lib/rockelivery/users/delete.ex
@@ -1,7 +1,15 @@
 defmodule Rockelivery.Users.Delete do
+  alias Ecto.UUID
   alias Rockelivery.{Error, Repo, User}
 
   def call(id) do
+    case UUID.cast(id) do
+      :error -> {:error, "Invalid ID format!"}
+      {:ok, uuid} -> delete(uuid)
+    end
+  end
+
+  defp delete(id) do
     case Repo.get(User, id) do
       nil -> {:error, Error.build_user_not_found_error()}
       user -> Repo.delete(user)
--- lib/rockelivery/users/get.ex
+++ lib/rockelivery/users/get.ex
@@ -1,7 +1,15 @@
 defmodule Rockelivery.Users.Get do
+  alias Ecto.UUID
   alias Rockelivery.{Error, Repo, User}
 
-  def get(id) do
+  def call(id) do
+    case UUID.cast(id) do
+      :error -> {:error, "Invalid ID format!"}
+      {:ok, uuid} -> get(uuid)
+    end
+  end
+
+  defp get(id) do
     case Repo.get(User, id) do
       nil -> {:error, Error.build_user_not_found_error()}
       user -> {:ok, user}
