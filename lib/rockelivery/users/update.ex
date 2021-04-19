b60e152fe1d8bd4cafe7204b6723c87d38f73ca6
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-04-19 06:03:11 -0300

    refactoring, cast was missing

--- lib/rockelivery/users/update.ex
+++ lib/rockelivery/users/update.ex
@@ -1,7 +1,15 @@
 defmodule Rockelivery.Users.Update do
+  alias Ecto.UUID
   alias Rockelivery.{Error, Repo, User}
 
   def call(%{"id" => id} = params) do
+    case UUID.cast(id) do
+      :error -> {:error, "Invalid ID format!"}
+      {:ok, _uuid} -> update(params)
+    end
+  end
+
+  defp update(%{"id" => id} = params) do
     case Repo.get(User, id) do
       nil -> {:error, Error.build_user_not_found_error()}
       user -> do_update(user, params)
