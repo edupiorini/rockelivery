75db7fedcd7ebaaa09137b8d2ae887901d2b0dee
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-05-11 07:44:42 -0300

    refactoring for validation before api

--- lib/rockelivery/users/create.ex
+++ lib/rockelivery/users/create.ex
@@ -1,16 +1,22 @@
 defmodule Rockelivery.Users.Create do
   alias Rockelivery.{Error, Repo, User}
+  alias Rockelivery.ViaCep.Client
 
-  def call(params) do
-    params
-    |> User.changeset()
-    |> Repo.insert()
-    |> handle_insert()
-  end
+  def call(%{"cep" => cep} = params) do
+    changeset = User.changeset(params)
 
-  defp handle_insert({:ok, %User{}} = result), do: result
+    with {:ok, %User{}} <- User.build(changeset),
+         {:ok, _cep_info} <- Client.get_cep_info(cep),
+         {:ok, %User{}} = user <- Repo.insert(changeset) do
+      user
+    else
+      {:error, %Error{}} = error -> error
+      {:error, result} -> {:error, Error.build(:bad_request, result)}
+    end
 
-  defp handle_insert({:error, result}) do
-    {:error, Error.build(:bad_request, result)}
+    # case Client.get_cep_info(cep) do
+    #   {:ok, _cep_info} -> create_user(params)
+    #   {:error, _result} = error -> error
+    # end
   end
 end
