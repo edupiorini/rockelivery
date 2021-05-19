1dec8262acff8694c94042a9a16c888690bd561b
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-05-19 06:13:20 -0300

    new factory for cep info

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -5,13 +5,13 @@ defmodule Rockelivery.Factory do
 
   def user_params_factory do
     %{
-      age: 28,
-      adress: "rua de teste, 25",
-      cep: "12070350",
-      cpf: "37779593835",
-      email: "teste@teste.com",
-      password: "123456",
-      name: "Eduardo"
+      "age" => 28,
+      "adress" => "rua de teste, 25",
+      "cep" => "12070350",
+      "cpf" => "37779593835",
+      "email" => "teste@teste.com",
+      "password" => "123456",
+      "name" => "Eduardo"
     }
   end
 
@@ -37,6 +37,21 @@ defmodule Rockelivery.Factory do
       password: "123456",
       name: "Eduardo",
       id: "58edadfd-460a-4698-9ef2-195b67eca239"
+    }
+  end
+
+  def cep_info_factory do
+    %{
+      "bairro" => "Sé",
+      "cep" => "01001-000",
+      "complemento" => "lado ímpar",
+      "ddd" => "11",
+      "gia" => "1004",
+      "ibge" => "3550308",
+      "localidade" => "São Paulo",
+      "logradouro" => "Praça da Sé",
+      "siafi" => "7107",
+      "uf" => "SP"
     }
   end
 end
