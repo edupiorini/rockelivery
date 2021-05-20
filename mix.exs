091e838337e07b7cfaf5bb8f3edb643b6ac56616
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-05-20 04:59:53 -0300

    pbkdf update

--- mix.exs
+++ mix.exs
@@ -52,7 +52,7 @@ defmodule Rockelivery.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:pbkdf2_elixir, "~> 1.3"},
+      {:pbkdf2_elixir, "~> 1.4"},
       {:excoveralls, "~> 0.10", only: :test},
       {:ex_machina, "~> 2.7.0"},
       {:tesla, "~> 1.4.0"},
