1852e889a0df9914ab9cc0e5af3c9acd7cdda220
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-06-16 05:35:28 -0300

    tes fixedt

--- test/rockelivery/users/create_test.exs
+++ test/rockelivery/users/create_test.exs
@@ -29,7 +29,7 @@ defmodule Rockelivery.Users.CreateTest do
 
       expected_response = %{
         age: ["must be greater than or equal to 18"],
-        password: ["should be at least 9 character(s)"]
+        password: ["should be at least 6 character(s)"]
       }
 
       assert {:error, %Error{status: :bad_request, result: changeset}} = response
